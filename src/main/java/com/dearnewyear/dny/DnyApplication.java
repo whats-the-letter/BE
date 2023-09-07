6a7d938ba4e4ba19ea5df38d044a3ce84c5e9cfd
Hyeonji-Jung <welloff.jj@gmail.com>
2023-09-07 18:44:46 +0900

    chore : SpringBoot 프로젝트 세팅

---
+++ src/main/java/com/dearnewyear/dny/DnyApplication.java
@@ -0,0 +1,13 @@
+package com.dearnewyear.dny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DnyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DnyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dearnewyear/dny/DnyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dearnewyear.dny;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DnyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
