b2dbe1cbcde46622646874b3f90e66ff2f92160e
Hyeonji-Jung <welloff.jj@gmail.com>
2023-10-05 21:54:25 +0900

    chore : ErrorResponse 구조 수정

--- src/main/java/com/dearnewyear/dny/common/dto/response/ErrorResponse.java
+++ src/main/java/com/dearnewyear/dny/common/dto/response/ErrorResponse.java
@@ -1,8 +1,10 @@
 package com.dearnewyear.dny.common.dto.response;
 
+import com.dearnewyear.dny.common.error.ErrorCode;
+
 public record ErrorResponse(Integer status, String message, Object data) {
 
-    public static ErrorResponse of(Integer status, String message, Object data) {
-        return new ErrorResponse(status, message, data);
+    public static ErrorResponse of(ErrorCode errorCode) {
+        return new ErrorResponse(errorCode.getStatus(), errorCode.getMessage(), null);
     }
 }
