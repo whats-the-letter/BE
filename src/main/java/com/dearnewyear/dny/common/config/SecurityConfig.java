24068b88680d695427b62822fa4d5950e557aecd
Hyeonji-Jung <welloff.jj@gmail.com>
2023-11-02 16:07:57 +0900

    feat: Security Config 초기 설정

---
+++ src/main/java/com/dearnewyear/dny/common/config/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.dearnewyear.dny.common.config;
+
+import com.dearnewyear.dny.common.jwt.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .headers().frameOptions().disable()
+                .and()
+                .cors()
+                .and()
+                .csrf().disable()
+                .authorizeHttpRequests()
+                .antMatchers("/oauth/**").authenticated()
+                .anyRequest().permitAll()
+                .and()
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
+    }
+}
