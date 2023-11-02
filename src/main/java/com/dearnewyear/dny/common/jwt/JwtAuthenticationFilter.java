c8c64c2c6e277d01fd4d151e8b92c0c38b4587cb
Hyeonji-Jung <welloff.jj@gmail.com>
2023-11-02 17:10:57 +0900

    chore: jwtAuthFilter에서 accessToken과 refreshToken 모두 확인하도록 수정

---
+++ src/main/java/com/dearnewyear/dny/common/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,46 @@
+package com.dearnewyear.dny.common.jwt;
+
+import java.io.IOException;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@RequiredArgsConstructor
+@Slf4j
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+            FilterChain filterChain) throws ServletException, IOException {
+        log.info("doFilterInternal 메소드 실행");
+
+        String accessToken = jwtTokenProvider.getAccessToken(request);
+        String refreshToken = jwtTokenProvider.getRefreshToken(request);
+
+        log.info("accessToken: " + accessToken);
+        log.info("refreshToken: " + refreshToken);
+
+        if (accessToken != null && jwtTokenProvider.validateToken(accessToken)) {
+            log.info("액세스 토큰 유효");
+            setAuthentication(accessToken);
+        }
+        if (refreshToken != null && jwtTokenProvider.validateToken(refreshToken)) {
+            log.info("리프레시 토큰 유효");
+            setAuthentication(refreshToken);
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private void setAuthentication(String accessToken) {
+        Authentication auth = jwtTokenProvider.getAuthentication(accessToken);
+        SecurityContextHolder.getContext().setAuthentication(auth);
+    }
+}
