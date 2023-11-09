9d96de27d850cd28a79bb0f35f0532b516f80371
Hyeonji-Jung <welloff.jj@gmail.com>
2023-11-09 22:08:57 +0900

    chore: CustomUserDetail 구현을 위한 Bean 등록

--- src/main/java/com/dearnewyear/dny/user/domain/User.java
+++ src/main/java/com/dearnewyear/dny/user/domain/User.java
@@ -1,16 +1,34 @@
 package com.dearnewyear.dny.user.domain;
 
 import javax.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
 
 @Entity
 @Table(name = "USER")
 @Getter
 @NoArgsConstructor
+@Component
 public class User {
 
     @Id @Column(name = "user_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private String userId;
+    private Long userId;
+
+    @Column(name = "user_name", nullable = false)
+    private String userName;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
+    private RefreshToken refreshToken;
+
+    @Builder
+    public User(String userName, String email, RefreshToken refreshToken) {
+        this.userName = userName;
+        this.email = email;
+    }
 }
