e1e3eec1f8f4550979872ca716092815669739f2
Hyeonji-Jung <welloff.jj@gmail.com>
2023-11-09 23:54:44 +0900

    chore: signup 엔드포인트 추가 및 CustomException handling 추가

--- src/main/java/com/dearnewyear/dny/user/controller/OauthController.java
+++ src/main/java/com/dearnewyear/dny/user/controller/OauthController.java
@@ -1,6 +1,8 @@
 package com.dearnewyear.dny.user.controller;
 
 import com.dearnewyear.dny.common.dto.response.ApiResponse;
+import com.dearnewyear.dny.common.dto.response.ErrorResponse;
+import com.dearnewyear.dny.common.error.exception.CustomException;
 import com.dearnewyear.dny.user.service.KakaoOAuth2Service;
 import java.io.IOException;
 import javax.servlet.http.HttpServletResponse;
@@ -20,25 +22,28 @@ public class OauthController {
     private final KakaoOAuth2Service kakaoOAuth2Service;
 
     @GetMapping("/login/kakao")
-    public RedirectView getAuthorizationCode(HttpServletResponse response) throws IOException {
+    public RedirectView getAuthorizationCode() throws IOException {
         String authorizationUri = kakaoOAuth2Service.getAuthorizationUri();
         return new RedirectView(authorizationUri);
     }
 
     @GetMapping("/login/kakao/code")
     public ResponseEntity<ApiResponse> kakaoLogin(@RequestParam("code") String code, HttpServletResponse response) {
-        String accessToken = kakaoOAuth2Service.getAccessToken(code);
-        response.setHeader("Content-Type", "application/json");
+        try {
+            response.setHeader("Content-Type", "application/json");
+            String accessToken = kakaoOAuth2Service.getAccessToken(code);
+            String email = kakaoOAuth2Service.getKakaoUser(accessToken, response);
 
-        if (kakaoOAuth2Service.getKakaoUser(accessToken, response)) {
-            System.out.println("AccessToken: " + response.getHeader("Authorization"));
-            System.out.println("RefreshToken: " + response.getHeader("RefreshToken"));
-
-            ApiResponse res = new ApiResponse(200, "카카오 로그인 성공", null);
-            return ResponseEntity.ok(res);
-        } else {
-            ApiResponse res = new ApiResponse(401, "회원가입 필요", null);
-            return ResponseEntity.status(401).body(res);
+            if (email.equals("success")) {
+                ApiResponse res = new ApiResponse(200, "카카오 로그인 성공", null);
+                return ResponseEntity.ok(res);
+            } else {
+                ApiResponse res = new ApiResponse(401, "회원가입 필요", email);
+                return ResponseEntity.status(401).body(res);
+            }
+        } catch (CustomException e) {
+            ErrorResponse res = ErrorResponse.of(e.getErrorCode());
+            return ResponseEntity.status(e.getErrorCode().getStatus()).body(res);
         }
     }
 }
--- src/main/java/com/dearnewyear/dny/user/controller/UserController.java
+++ src/main/java/com/dearnewyear/dny/user/controller/UserController.java
@@ -1,9 +1,34 @@
 package com.dearnewyear.dny.user.controller;
 
+import com.dearnewyear.dny.common.dto.response.ApiResponse;
+import com.dearnewyear.dny.common.dto.response.ErrorResponse;
+import com.dearnewyear.dny.common.error.exception.CustomException;
+import com.dearnewyear.dny.user.dto.request.SignupRequest;
+import com.dearnewyear.dny.user.service.UserService;
+import javax.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/user")
 public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<ApiResponse> signup(@RequestBody SignupRequest request, HttpServletResponse response) {
+        try {
+            userService.signupAndLoginUser(request, response);
+            ApiResponse res = new ApiResponse(200, "회원가입 성공", null);
+            return ResponseEntity.ok(res);
+        } catch (CustomException e) {
+            ErrorResponse res = ErrorResponse.of(e.getErrorCode());
+            return ResponseEntity.status(e.getErrorCode().getStatus()).body(res);
+        }
+    }
 }
