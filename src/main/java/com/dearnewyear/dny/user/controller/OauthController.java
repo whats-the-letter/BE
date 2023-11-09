4a49d1df4f8cd5878bf8ae149c0f16233c798812
Hyeonji-Jung <welloff.jj@gmail.com>
2023-11-09 22:06:36 +0900

    feat: kakao oauth2 로그인 및 서비스 회원 판별 로직 구현
    - 드디어 !!!!!!!!!

--- src/main/java/com/dearnewyear/dny/user/controller/OauthController.java
+++ src/main/java/com/dearnewyear/dny/user/controller/OauthController.java
@@ -1,42 +1,44 @@
 package com.dearnewyear.dny.user.controller;
 
 import com.dearnewyear.dny.common.dto.response.ApiResponse;
-import com.dearnewyear.dny.common.jwt.JwtTokenProvider;
-import com.dearnewyear.dny.user.domain.User;
 import com.dearnewyear.dny.user.service.KakaoOAuth2Service;
-import com.fasterxml.jackson.core.JsonProcessingException;
+import java.io.IOException;
 import javax.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/oauth")
 public class OauthController {
 
-    private final JwtTokenProvider jwtTokenProvider;
     private final KakaoOAuth2Service kakaoOAuth2Service;
 
-    @RequestMapping("/kakao")
-    public ResponseEntity<ApiResponse> kakaoLogin(@RequestParam String code, HttpServletResponse response)
-            throws JsonProcessingException {
+    @GetMapping("/login/kakao")
+    public RedirectView getAuthorizationCode(HttpServletResponse response) throws IOException {
+        String authorizationUri = kakaoOAuth2Service.getAuthorizationUri();
+        return new RedirectView(authorizationUri);
+    }
 
-        String token = kakaoOAuth2Service.getAccessToken(code);
-        User kakaoUser = kakaoOAuth2Service.getKakaoUser(token);
+    @GetMapping("/login/kakao/code")
+    public ResponseEntity<ApiResponse> kakaoLogin(@RequestParam("code") String code, HttpServletResponse response) {
+        String accessToken = kakaoOAuth2Service.getAccessToken(code);
+        response.setHeader("Content-Type", "application/json");
 
-        String accessToken = jwtTokenProvider.createAccessToken(kakaoUser);
-        String refreshToken = jwtTokenProvider.createRefreshToken(kakaoUser);
+        if (kakaoOAuth2Service.getKakaoUser(accessToken, response)) {
+            System.out.println("AccessToken: " + response.getHeader("Authorization"));
+            System.out.println("RefreshToken: " + response.getHeader("RefreshToken"));
 
-        response.setHeader("Authorization", accessToken);
-        response.setHeader("RefreshToken", refreshToken);
-
-        return ResponseEntity.ok(ApiResponse.builder()
-                .status(200)
-                .message("카카오 로그인 성공")
-                .data(null)
-                .build());
+            ApiResponse res = new ApiResponse(200, "카카오 로그인 성공", null);
+            return ResponseEntity.ok(res);
+        } else {
+            ApiResponse res = new ApiResponse(401, "회원가입 필요", null);
+            return ResponseEntity.status(401).body(res);
+        }
     }
 }
---
+++ src/main/java/com/dearnewyear/dny/user/service/KakaoOAuth2Service.java
@@ -0,0 +1,89 @@
+package com.dearnewyear.dny.user.service;
+
+import com.dearnewyear.dny.common.error.ErrorCode;
+import com.dearnewyear.dny.common.error.exception.CustomException;
+import java.util.Map;
+import javax.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+@RequiredArgsConstructor
+public class KakaoOAuth2Service {
+
+    @Value("${spring.security.oauth2.client.registration.kakao.client-id}")
+    private String kakaoClientId;
+
+    @Value("${spring.security.oauth2.client.registration.kakao.client-secret}")
+    private String kakaoClientSecret;
+
+    @Value("${spring.security.oauth2.client.registration.kakao.redirect-uri}")
+    private String kakaoRedirectUri;
+
+    @Value("${spring.security.oauth2.client.provider.kakao.authorization-uri}")
+    private String kakaoAuthorizationUri;
+
+    @Value("${spring.security.oauth2.client.provider.kakao.token-uri}")
+    private String kakaoTokenUri;
+
+    @Value("${spring.security.oauth2.client.provider.kakao.user-info-uri}")
+    private String kakaoUserInfoUri;
+
+    private final UserService userService;
+
+    public String getAccessToken(String code) {
+        RestTemplate restTemplate = new RestTemplate();
+
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("client_id", kakaoClientId);
+        params.add("client_secret", kakaoClientSecret);
+        params.add("redirect_uri", kakaoRedirectUri);
+        params.add("code", code);
+        params.add("grant_type", "authorization_code");
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(params, headers);
+        ResponseEntity<Map> response = restTemplate.postForEntity(kakaoTokenUri, request, Map.class);
+
+        if (response.getStatusCode() == HttpStatus.OK) {
+            return (String) response.getBody().get("access_token");
+        } else {
+            throw new CustomException(ErrorCode.INVALID_USER_PARAMS);
+        }
+    }
+
+    public boolean getKakaoUser(String token, HttpServletResponse response) {
+        RestTemplate restTemplate = new RestTemplate();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", "Bearer " + token);
+        headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+        ResponseEntity<Map> kakaoResponse = restTemplate.postForEntity(kakaoUserInfoUri, new HttpEntity<>(headers), Map.class);
+
+        if (kakaoResponse.getStatusCode() == HttpStatus.OK) {
+            Map<String, Object> kakaoAccount = (Map<String, Object>) kakaoResponse.getBody().get("kakao_account");
+            String email = (String) kakaoAccount.get("email");
+
+            System.out.println("email: " + email);
+
+            return userService.chkAndLoginUser(email, response);
+        } else {
+            throw new CustomException(ErrorCode.KAKAO_OAUTH2_ERROR);
+        }
+    }
+
+    public String getAuthorizationUri() {
+        return kakaoAuthorizationUri + "?client_id=" + kakaoClientId + "&redirect_uri=" + kakaoRedirectUri + "&response_type=code";
+    }
+}
