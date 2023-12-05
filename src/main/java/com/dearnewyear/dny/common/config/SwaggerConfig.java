42eca974948a70ab0ede39e1749f0bc7b245a384
Hyeonji-Jung <welloff.jj@gmail.com>
2023-12-05 16:53:03 +0900

    fix: swagger와 spring security 연동을 위한 수정
    - springfox는 spring MVC와 함께 동작하도록 설정되어 있어서 별도로 @EnableWebMvc를 사용할 필요 없다고 함 ..
    - swagger 2 버전에 맞는 링크들 다 security config 파일에서 열어둠

--- src/main/java/com/dearnewyear/dny/common/config/SwaggerConfig.java
+++ src/main/java/com/dearnewyear/dny/common/config/SwaggerConfig.java
@@ -1,8 +1,8 @@
 package com.dearnewyear.dny.common.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -11,24 +11,40 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
-@EnableWebMvc
 public class SwaggerConfig {
+
+    @Value("${swagger.base-package}")
+    private String BASE_PACKAGE;
+
+    @Value("${swagger.path}")
+    private String API_PATH;
+
+    @Value("${swagger.title}")
+    private String API_TITLE;
+
+    @Value("${swagger.description}")
+    private String API_DESCRIPTION;
+
+    @Value("${swagger.version}")
+    private String API_VERSION;
 
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage(BASE_PACKAGE))
+                .paths(PathSelectors.ant(API_PATH))
                 .build()
-                .apiInfo(apiInfo());
+                .apiInfo(apiInfo())
+                .useDefaultResponseMessages(false)
+                .pathMapping("/");
     }
 
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-                .title("DNY API")
-                .description("DNY API Reference for Yeokyoung")
-                .version("1.0")
+                .title(API_TITLE)
+                .description(API_DESCRIPTION)
+                .version(API_VERSION)
                 .build();
     }
 }
