2a48eccf002c51ea528fd1ae6063306e23e85615
Hyeonji-Jung <welloff.jj@gmail.com>
2023-12-10 21:24:04 +0900

    chore: CORS Filter 추가

--- src/main/java/com/dearnewyear/dny/common/config/SecurityConfig.java
+++ src/main/java/com/dearnewyear/dny/common/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.dearnewyear.dny.common.config;
 
 import com.dearnewyear.dny.common.jwt.JwtAuthenticationFilter;
 import com.dearnewyear.dny.common.jwt.JwtTokenProvider;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +10,9 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -16,6 +20,18 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class SecurityConfig {
 
     private final JwtTokenProvider jwtTokenProvider;
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(false);
+        config.setAllowedOrigins(List.of("*"));
+        config.setAllowedHeaders(List.of("Authorization", "DNY-Refresh", "Content-Type"));
+        config.setAllowedMethods(List.of("GET", "POST"));
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
