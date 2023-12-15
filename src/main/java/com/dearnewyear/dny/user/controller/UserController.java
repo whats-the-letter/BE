580b56006cb53045dd113bfad1718572dacc3885
Hyeonji-Jung <welloff.jj@gmail.com>
2023-12-15 15:12:29 +0900

    feat: 다른 사용자 정보 조회 구현

--- src/main/java/com/dearnewyear/dny/user/controller/UserController.java
+++ src/main/java/com/dearnewyear/dny/user/controller/UserController.java
@@ -1,7 +1,16 @@
 package com.dearnewyear.dny.user.controller;
 
+import com.dearnewyear.dny.common.error.exception.CustomException;
+import com.dearnewyear.dny.user.dto.UserInfo;
+import com.dearnewyear.dny.user.dto.response.UserInfoResponse;
+import com.dearnewyear.dny.user.service.UserService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +19,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/user")
 public class UserController {
+
+    private final UserService userService;
+
+    @ApiOperation(value = "다른 사용자 메인 정보")
+    @ApiResponses({
+            @io.swagger.annotations.ApiResponse(code = 200, message = "메인 조회 성공"),
+            @io.swagger.annotations.ApiResponse(code = 400, message = "메인 조회 권한 없음"),
+            @io.swagger.annotations.ApiResponse(code = 404, message = "메인 조회 실패")
+    })
+    @GetMapping("/main/{userId}")
+    public ResponseEntity<UserInfoResponse> getMainInfo(@PathVariable Long userId) {
+        try {
+            UserInfo userInfo = userService.getOtherUserInfo(userId);
+            return ResponseEntity.ok(new UserInfoResponse(userInfo, null));
+        } catch (CustomException e) {
+            return ResponseEntity.status(e.getErrorCode().getStatus()).body(new UserInfoResponse(null, e.getMessage()));
+        }
+    }
 }
