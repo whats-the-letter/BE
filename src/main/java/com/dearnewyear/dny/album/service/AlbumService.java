6ec538a5f2eb911c03f3f669447e59dbcec26ead
Hyeonji-Jung <welloff.jj@gmail.com>
2024-05-05 01:19:11 +0900

    refac: 중복 코드 제거 및 리팩토링

--- src/main/java/com/dearnewyear/dny/album/service/AlbumService.java
+++ src/main/java/com/dearnewyear/dny/album/service/AlbumService.java
@@ -13,7 +13,6 @@ import com.dearnewyear.dny.music.repository.MusicRepository;
 import com.dearnewyear.dny.user.domain.User;
 import com.dearnewyear.dny.user.repository.UserRepository;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 import javax.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
@@ -34,14 +33,10 @@ public class AlbumService {
 
     public AlbumInfo sendAlbum(AlbumRequest albumRequest, HttpServletRequest request) {
         String accessToken = jwtTokenProvider.getAccessToken(request);
-        User fromUser = userRepository.findById(jwtTokenProvider.getUserId(accessToken))
-                .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
 
-        String toUserId = albumRequest.getToUserId();
-        User toUser = null;
-        if (toUserId != null)
-            toUser = userRepository.findById(toUserId)
-                    .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
+        User fromUser = findUserById(jwtTokenProvider.getUserId(accessToken));
+        User toUser = findUserByIdOrNull(albumRequest.getToUserId());
+        Music music = findMusicById(albumRequest.getMusicId());
 
         String frontImgName = s3Service.upload(albumRequest.getFrontImage());
         String backImgName = s3Service.upload(albumRequest.getBackImage());
@@ -50,9 +45,9 @@ public class AlbumService {
                 .albumCover(albumRequest.getAlbumCover())
                 .albumPhrases(albumRequest.getAlbumPhrases())
                 .albumBackground(albumRequest.getAlbumBackground())
-                .musicId(albumRequest.getMusicId())
+                .musicId(music.getMusicId())
                 .fromUserId(fromUser.getUserId())
-                .toUserId(toUserId)
+                .toUserId(toUser != null ? toUser.getUserId() : null)
                 .fromName(albumRequest.getFromName())
                 .toName(albumRequest.getToName())
                 .letter(albumRequest.getLetter())
@@ -64,53 +59,45 @@ public class AlbumService {
     }
 
     public AlbumInfo viewAlbum(String albumId, HttpServletRequest request) {
-        Album album = albumRepository.findById(albumId)
-                .orElseThrow(() -> new CustomException(ErrorCode.ALBUM_NOT_FOUND));
-
         String accessToken = jwtTokenProvider.getAccessToken(request);
+
+        Album album = findAlbumById(albumId);
         Authentication authentication = jwtTokenProvider.getAuthentication(accessToken);
         checkAlbumPermission(album, authentication);
 
-        return new AlbumInfo(album, musicRepository.findById(album.getMusicId())
-                .orElseThrow(() -> new CustomException(ErrorCode.MUSIC_NOT_FOUND)));
+        return new AlbumInfo(album, findMusicById(album.getMusicId()));
     }
 
     public List<AlbumInfo> viewCollection(HttpServletRequest request) {
         String accessToken = jwtTokenProvider.getAccessToken(request);
-        User user = userRepository.findById(jwtTokenProvider.getUserId(accessToken))
-                .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
 
+        User user = findUserById(jwtTokenProvider.getUserId(accessToken));
         List<Album> albumList = albumRepository.findByToUserId(user.getUserId());
         return albumList.stream()
-                .map(album -> new AlbumInfo(album, musicRepository.findById(album.getMusicId())
-                        .orElseThrow(() -> new CustomException(ErrorCode.MUSIC_NOT_FOUND)))
-                ).collect(Collectors.toList());
+                .map(album -> new AlbumInfo(album, findMusicById(album.getMusicId())))
+                .collect(Collectors.toList());
     }
 
     public void addAlbumToCollection(String albumId, HttpServletRequest request) {
-        Album album = albumRepository.findById(albumId)
-                .orElseThrow(() -> new CustomException(ErrorCode.ALBUM_NOT_FOUND));
+        String accessToken = jwtTokenProvider.getAccessToken(request);
+
+        Album album = findAlbumById(albumId);
         if (album.getToUserId() != null)
             throw new CustomException(ErrorCode.ALBUM_NOT_AUTHORIZED);
 
-        String accessToken = jwtTokenProvider.getAccessToken(request);
-        User user = userRepository.findById(jwtTokenProvider.getUserId(accessToken))
-                .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
-
+        User user = findUserById(jwtTokenProvider.getUserId(accessToken));
         album.updateTo(user.getUserId());
+        albumRepository.save(album);
     }
 
     private void checkAlbumPermission(Album album, Authentication authentication) {
         if (album.getToUserId() == null)
             return;
 
-        Optional<User> fromUser = userRepository.findById(album.getFromUserId());
-        Optional<User> toUser = userRepository.findById(album.getToUserId());
+        User fromUser = findUserById(album.getFromUserId());
+        User toUser = findUserByIdOrNull(album.getToUserId());
 
-        if (fromUser == null)
-            throw new CustomException(ErrorCode.USER_NOT_FOUND);
-
-        if (!isFromUser(fromUser.get(), authentication) && !isToUser(toUser.get(), authentication))
+        if (!isFromUser(fromUser, authentication) && (toUser != null && !isToUser(toUser, authentication)))
             throw new CustomException(ErrorCode.ALBUM_NOT_AUTHORIZED);
     }
 
@@ -120,5 +107,24 @@ public class AlbumService {
 
     private boolean isToUser(User toUser, Authentication authentication) {
         return toUser.getUserName().equals(authentication.getName());
+    }
+
+    public User findUserById(String userId) {
+        return userRepository.findById(userId)
+                .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
+    }
+
+    public User findUserByIdOrNull(String userId) {
+        return userId != null ? findUserById(userId) : null;
+    }
+
+    public Music findMusicById(String musicId) {
+        return musicRepository.findById(musicId)
+                .orElseThrow(() -> new CustomException(ErrorCode.MUSIC_NOT_FOUND));
+    }
+
+    public Album findAlbumById(String albumId) {
+        return albumRepository.findById(albumId)
+                .orElseThrow(() -> new CustomException(ErrorCode.ALBUM_NOT_FOUND));
     }
 }
