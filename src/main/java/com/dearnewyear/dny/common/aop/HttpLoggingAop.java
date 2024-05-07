ec88576fb8c84d54d1579705814fbe4ba82bd403
Hyeonji-Jung <welloff.jj@gmail.com>
2024-05-08 00:48:44 +0900

    chore: 로깅 수정
    - cloudwatch log stream 생성 옵션 삭제
    - HttpLoggingAop에서 정상적인 응답 반환된 경우만 로그를 남기도록 수정

--- src/main/java/com/dearnewyear/dny/common/aop/HttpLoggingAop.java
+++ src/main/java/com/dearnewyear/dny/common/aop/HttpLoggingAop.java
@@ -14,28 +14,18 @@ public class HttpLoggingAop {
 
     @Around("execution(* com.dearnewyear.dny..controller..*.*(..))")
     public Object logHttpAround(ProceedingJoinPoint joinPoint) throws Throwable {
-        log.info("Executing: {}", joinPoint.getSignature());
-        try {
-            Object result = joinPoint.proceed();
-            if (result instanceof ResponseEntity) {
-                logResponse((ResponseEntity) result);
-                return result;
-            }
-            log.info("Result: {}", result);
-            return result;
-        } catch (Exception e) {
-            log.error("Exception: ", e);
-            throw e;
+        String methodName = joinPoint.getSignature().toShortString();
+        Object result = joinPoint.proceed();
+        if (result instanceof ResponseEntity) {
+            logResponse((ResponseEntity) result);
+        } else {
+            log.info("{} returned: {}", methodName, result);
         }
+        return result;
     }
 
     private void logResponse(ResponseEntity responseEntity) {
-        if (!responseEntity.getStatusCode().is2xxSuccessful()) {
-            log.info("ErrorCode: {}", responseEntity.getStatusCode());
-            log.info("ErrorMessage: {}", responseEntity.getBody());
-            return;
-        }
-        log.info("StatusCode: {}", responseEntity.getStatusCode());
-        log.info("Response: {}", responseEntity.getBody());
+        String status = responseEntity.getStatusCode().toString();
+        log.info("Response: {} - {}", status, responseEntity.getBody());
     }
 }
