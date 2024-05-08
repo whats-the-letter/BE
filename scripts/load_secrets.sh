b619e2182faab5d9ff3576411b2875215952452c
Hyeonji-Jung <welloff.jj@gmail.com>
2024-05-08 21:31:09 +0900

    fix: REGION 명시

--- scripts/load_secrets.sh
+++ scripts/load_secrets.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-export TIME=$(aws ssm get-parameter --name "/wtl/time" --query "Parameter.Value" --output text)
+export TIME=$(aws ssm get-parameter --name "/wtl/time" --query "Parameter.Value" --output text --region ap-northeast-2)
