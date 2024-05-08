ba0a57ed5fdb0da8ece03c5138f1b73512a41497
jijongkwon <help3451@naver.com>
2024-05-08 14:44:41 +0900

    fix: 예약된 변수명 수정

--- scripts/load_secrets.sh
+++ scripts/load_secrets.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-export AWS_REGION=$(aws ssm get-parameter --name "/aws/region" --with-decryption --query "Parameter.Value" --output text)
+export AWS_REGION=$(aws ssm get-parameter --name "/aws/myregion" --with-decryption --query "Parameter.Value" --output text)
 export TIME=$(aws ssm get-parameter --name "/app/time" --query "Parameter.Value" --output text)
