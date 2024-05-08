b1a153c6b1e194de017bb70f0a312c29aaf85b98
jijongkwon <help3451@naver.com>
2024-05-08 14:46:49 +0900

    fix: 파라미터명 수정

--- scripts/load_secrets.sh
+++ scripts/load_secrets.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-export AWS_REGION=$(aws ssm get-parameter --name "/aws/myregion" --with-decryption --query "Parameter.Value" --output text)
-export TIME=$(aws ssm get-parameter --name "/app/time" --query "Parameter.Value" --output text)
+export AWS_REGION=$(aws ssm get-parameter --name "/wtl/region" --with-decryption --query "Parameter.Value" --output text)
+export TIME=$(aws ssm get-parameter --name "/wtl/time" --query "Parameter.Value" --output text)
